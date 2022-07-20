dbc040c97fb9dc5c16ccb9f21ce1a7972f574edc
E K <emmanuelkoupoh@gmail.com>
2022-07-20 16:38:03 +0000

    utils with signature

---
+++ utils.py
@@ -0,0 +1,38 @@
+# This is the the file that must contain the functions and classes that will be used in the app
+
+import pandas as pd
+
+
+def preprocessing(data) -> pd.DataFrame:
+    """This is the function to apply the defined preprocessing.
+
+    Parameters
+    ----------
+    data : pd.DataFrame
+    The the dataframe containing the data to apply the preprocessing on it.
+
+    Returns
+    -------
+    prepocessed_data : pd.DataFrame
+    The the dataframe containing the data preprocessed.
+    """
+
+    return prepocessed_data
+
+
+def feature_engineering(data) -> pd.DataFrame:
+    """This is the function to apply the defined feature engineering to the data before
+    feeding the model with it.
+
+    Parameters
+    ----------
+    data : pd.DataFrame
+    The the dataframe containing the data to apply the feature engineering on it.
+
+    Returns
+    -------
+    adapted_data : pd.DataFrame
+    The the dataframe containing the data ready to prediction.
+    """
+
+    return adapted_data
